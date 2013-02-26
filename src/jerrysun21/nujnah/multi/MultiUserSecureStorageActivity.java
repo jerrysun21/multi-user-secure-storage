e4002d67d686b29add2d5f9fe0dea980b9aa4e77
Jerry Sun <j.x.y.sun@gmail.com>
2013-02-26 11:41:40 -0500

    Correctly reads user list, saves passwords as SHA1 hash

--- src/jerrysun21/nujnah/multi/MultiUserSecureStorageActivity.java
+++ src/jerrysun21/nujnah/multi/MultiUserSecureStorageActivity.java
@@ -3,12 +3,15 @@ package jerrysun21.nujnah.multi;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 
 import android.app.Activity;
@@ -21,22 +24,36 @@ import android.os.Environment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MultiUserSecureStorageActivity extends Activity {    
     
     final String strAppDir = "jerrysun21.nujnah.multi";
     final String strUserFile = "users";
-    ArrayList<String> users = new ArrayList<String>();
+    ArrayList<MultiUserInfo> users = new ArrayList<MultiUserInfo>();
+    Button btnTapNFC;
+    // TextView for the line of text in the main activity
+    TextView tvMainText;
+    ArrayAdapter<String> adapter;
+
     
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+                
+        btnTapNFC = (Button)findViewById(R.id.main_tap_NFC);
+        tvMainText = (TextView)findViewById(R.id.main_text);
+        
+        tvMainText.setVisibility(View.GONE);
+        btnTapNFC.setVisibility(View.GONE);
         
         String diskState = Environment.getExternalStorageState();
         
@@ -79,9 +96,11 @@ public class MultiUserSecureStorageActivity extends Activity {
         	if (users.size() == 0) {
         		Toast.makeText(getApplicationContext(), "No users found please create a user", Toast.LENGTH_SHORT).show();
         		createUserDialog(this, appDir);
+        		tvMainText.setVisibility(View.VISIBLE);
+        		tvMainText.setText("No users found, please create a new user");
+        	} else {
+        		btnTapNFC.setVisibility(View.VISIBLE);
         	}
-        	
-        	//if (diskState.equals(Environment.MEDIA_MOUNTED_READ_ONLY))
         }
     }
         
@@ -93,18 +112,21 @@ public class MultiUserSecureStorageActivity extends Activity {
     }
     
     // Probably move this into another thread, would get slow once there are a lot of users
-    private ArrayList<String> getUserList(File userFile) {
-    	ArrayList<String> list = new ArrayList<String> ();
+    private ArrayList<MultiUserInfo> getUserList(File userFile) {
+    	ArrayList<MultiUserInfo> list = new ArrayList<MultiUserInfo> ();
     	String line;
     	
     	try {
-    		InputStream inStream = this.getAssets().open(userFile.getAbsolutePath());
-    		BufferedReader reader = new BufferedReader(new InputStreamReader(inStream));
+    		FileInputStream fin = new FileInputStream(userFile);
+    		BufferedReader reader = new BufferedReader(new InputStreamReader(fin));
     		while ((line = reader.readLine()) != null) {
-    			list.add(line);
-    		reader.close();
-    		inStream.close();
+    			String[] temp;
+    			temp = line.split("-");
+    			MultiUserInfo tempUser = new MultiUserInfo(temp[0], temp[1], temp[2]);
+    			list.add(tempUser);
     		}
+    		reader.close();
+    		fin.close();
     	} catch (Exception e) {
     		e.printStackTrace();
     	}
@@ -128,8 +150,10 @@ public class MultiUserSecureStorageActivity extends Activity {
 			public void onClick(View v) {
 				// Handle password later
 				String username = usernameEdit.getText().toString();
-				users.add(username);
-				createUser(username, appDir);
+				String password = MultiHelper.toSHA1(passwordEdit.getText().toString());
+				String nfc = "NFC";		// Temporary
+				MultiUserInfo newUser = new MultiUserInfo(username, password, nfc);
+				createUser(newUser, appDir);
 				dialog.dismiss();
 			}
 		});
@@ -146,17 +170,26 @@ public class MultiUserSecureStorageActivity extends Activity {
     }
     
     // Appends username to file & creates a folder for the user
-    private void createUser(String username, final File appDir) {
-		File userFolder = new File (appDir.getAbsolutePath(), username);
-		// This folder is not being created
+    private void createUser(MultiUserInfo newUser, final File appDir) {
+		File userFolder = new File (appDir.getAbsolutePath(), newUser.getUserId());
 		userFolder.mkdir();
 		File userList = getFile(strUserFile, appDir.listFiles());
 		try {
 			BufferedWriter writer = new BufferedWriter(new FileWriter(userList, true));
-			// Append not working
-			writer.append(username);
+			writer.append(newUser.getUserId());
+			writer.append("-");
+			writer.append(newUser.getPassword());
+			writer.append("-");
+			writer.append(newUser.getNFCToken());
 			writer.newLine();
 			writer.close();
+			
+			// Display new list of users
+			if (tvMainText.getVisibility() == View.VISIBLE)
+				tvMainText.setVisibility(View.GONE);
+			if (btnTapNFC.getVisibility() == View.GONE)
+				btnTapNFC.setVisibility(View.VISIBLE);
+			users = getUserList(userList);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -164,12 +197,5 @@ public class MultiUserSecureStorageActivity extends Activity {
 		
 
     }
-    /*
-   		lv = (ListView)findViewById(R.id.tag_list);
-		ArrayList<String> tag_list = new ArrayList<String>();
-		for (int i = 0; i < 20; i++)
-			tag_list.add("Sidebar Item");		// test data for now
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.activity_tag_list_item , tag_list);
-		lv.setAdapter(adapter);
-     */
+
 }
