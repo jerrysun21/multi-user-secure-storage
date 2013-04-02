3c324e8331a6cd1b02a4b09900015bdcab1ae54e
Jerry Sun <j.x.y.sun@gmail.com>
2013-04-02 18:55:33 -0400

    file browser

--- src/jerrysun21/nujnah/multi/MultiUserFileAdapter.java
+++ src/jerrysun21/nujnah/multi/MultiUserFileAdapter.java
@@ -1,8 +1,10 @@
 package jerrysun21.nujnah.multi;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Typeface;
@@ -13,21 +15,27 @@ import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MultiUserFileAdapter extends ArrayAdapter<File> {
 
 	private Context context;
-	final private List<File> list;
+	private List<File> list;
 	private int lvResource;
-
+	private int type;
+	
 	public MultiUserFileAdapter(Context context, int textViewResourceId,
 			List<File> objects) {
+		this(context, textViewResourceId, objects, 0);
+		// TODO Auto-generated constructor stub
+	}
+	public MultiUserFileAdapter(Context context, int textViewResourceId,
+			List<File> objects, int type) {
 		super(context, textViewResourceId, objects);
 		// TODO Auto-generated constructor stub
 		this.context = context;
 		this.list = objects;
 		this.lvResource = textViewResourceId;
+		this.type = type;
 	}
 
 	@Override
@@ -45,12 +53,17 @@ public class MultiUserFileAdapter extends ArrayAdapter<File> {
 
 		if (currentFile.isDirectory())
 			tv.setTypeface(Typeface.DEFAULT_BOLD);
+		else
+			tv.setTypeface(Typeface.DEFAULT);
+
+		if (position == 0 && type == 1) {
+			tv.setText("..");
+		} 
 
 		view.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
-				Toast.makeText(context, "clicked", Toast.LENGTH_SHORT).show();
 				String item = ((TextView) v.findViewById(R.id.item_name))
 						.getText().toString();
 				clickFile(item);
@@ -66,16 +79,40 @@ public class MultiUserFileAdapter extends ArrayAdapter<File> {
 
 		for (int i = 0; i < list.size(); i++) {
 			File temp = list.get(i);
-			if (temp.getName().equals(item) && temp.isDirectory())
+			if (temp.getName().equals(item) && temp.isDirectory()) {
 				userdir = list.get(i);
+				break;
+			} else if (item.equals("..")) {
+				if (i == 0) {
+					userdir = temp; 		// Going up a level
+					break;
+				}
+			}
 		}
 
-		if (userdir != null) {
+		if (userdir != null && type == 0) {
 			data.putString("userdir", userdir.getAbsolutePath());
 
 			Intent intent = new Intent(context, UserFolderBrowser.class);
 			intent.putExtras(data);
 			context.startActivity(intent);
+		} else if (userdir != null && type == 1) {
+			// navigate to folder instead
+			if (userdir.getAbsolutePath().equals("/storage/emulated/0/data/jerrysun21.nujnah.multi")) {
+				((Activity)context).finish();
+			} else {
+				clear();
+				File[] files = userdir.listFiles();
+				ArrayList<File> newFiles = new ArrayList<File>();
+				add(userdir.getParentFile());
+				newFiles.add(userdir.getParentFile());
+				for (int i = 0; i < files.length; i++) {
+					newFiles.add(files[i]);
+					add(files[i]);
+				}
+				list = newFiles;
+				notifyDataSetChanged();
+			}
 		}
 	}
 }
--- src/jerrysun21/nujnah/multi/UserFolderBrowser.java
+++ src/jerrysun21/nujnah/multi/UserFolderBrowser.java
@@ -1,24 +1,64 @@
 package jerrysun21.nujnah.multi;
 
 import java.io.File;
+import java.util.ArrayList;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class UserFolderBrowser extends Activity {
 	File UserDir;
 	String strUserDir;
+	MultiUserFileAdapter adapter;
+	TextView status;
+	ListView lv;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_folder_browser);
 		Bundle data = getIntent().getExtras();
+		status = (TextView)findViewById(R.id.ufb_status_text);
+		lv = (ListView)findViewById(R.id.ufb_file_list);
 		
 		if (data != null) {
 			strUserDir = data.getString("userdir");
 		}
 		UserDir = new File(strUserDir);
 		
+		if (UserDir.listFiles().length > 0 ) {
+			status.setVisibility(View.GONE);
+		} else {
+			status.setVisibility(View.VISIBLE);
+			status.setText("Empty Folder");
+		}
+		
+		showFileList(UserDir);
+	}
+	
+	private ArrayList<File> populateFileList(File dir) {
+		if (!dir.isDirectory()) {
+			return null;
+		} else {
+			ArrayList<File> list = new ArrayList<File>();
+			list.add(dir.getParentFile());
+			for (int i = 0; i < dir.listFiles().length; i++) {
+				File temp = dir.listFiles()[i];
+				list.add(temp);
+			}
+			return list;
+		}
+	}
+	
+	private void showFileList(File dir) {
+		if (!dir.isDirectory())
+			return;
+		
+		ArrayList<File> fileList = populateFileList(dir);
+		adapter = new MultiUserFileAdapter(this, R.layout.folder_list_item, fileList, 1);
+		lv.setAdapter(adapter);
 	}
 }
