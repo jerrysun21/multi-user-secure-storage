cd6cf1c4f3fe63b431e53bd9d820950ab3899bbf
Hanjun Kim <hanjun.j.kim@gmail.com>
2013-04-02 19:51:27 -0400

    Organized imports

--- src/jerrysun21/nujnah/multi/MultiUserFileAdapter.java
+++ src/jerrysun21/nujnah/multi/MultiUserFileAdapter.java
@@ -11,7 +11,6 @@ import android.content.Intent;
 import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
