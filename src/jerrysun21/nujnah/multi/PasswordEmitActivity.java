da92e1c18340b70c345eecac50257264d05a1aec
Hanjun Kim <hanjun.j.kim@gmail.com>
2013-04-02 16:31:20 -0400

    NFC functionality working

--- src/jerrysun21/nujnah/multi/PasswordEmitActivity.java
+++ src/jerrysun21/nujnah/multi/PasswordEmitActivity.java
@@ -1,16 +1,14 @@
 package jerrysun21.nujnah.multi;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.nfc.NdefMessage;
-import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
-import android.nfc.NfcAdapter.CreateNdefMessageCallback;
-import android.nfc.NfcEvent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.widget.TextView;
-import android.widget.Toast;
 
-public class PasswordEmitActivity extends Activity implements CreateNdefMessageCallback {
+public class PasswordEmitActivity extends Activity {
 	NfcAdapter mNfcAdapter;
     TextView textView;
 
@@ -19,34 +17,28 @@ public class PasswordEmitActivity extends Activity implements CreateNdefMessageC
         super.onCreate(savedInstanceState);
         setContentView(R.layout.password_emit);
         textView = (TextView) findViewById(R.id.textView);
-        // Check for available NFC Adapter
-        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
-        if (mNfcAdapter == null) {
-            Toast.makeText(this, "NFC is not available", Toast.LENGTH_LONG).show();
-            finish();
-            return;
-        }
-        // Register callback
-        mNfcAdapter.setNdefPushMessageCallback(this, this);
     }
-
+    
     @Override
-    public NdefMessage createNdefMessage(NfcEvent event) {
-        String text = ("Beam me up, Android!\n\n" +
-                "Beam Time: " + System.currentTimeMillis());
-        NdefMessage msg = new NdefMessage(
-                new NdefRecord[] { NdefRecord.createMime(
-                        "text/password", text.getBytes())
-         /**
-          * The Android Application Record (AAR) is commented out. When a device
-          * receives a push with an AAR in it, the application specified in the AAR
-          * is guaranteed to run. The AAR overrides the tag dispatch system.
-          * You can add it back in to guarantee that this
-          * activity starts when receiving a beamed message. For now, this code
-          * uses the tag dispatch system.
-          */
-          //,NdefRecord.createApplicationRecord("com.example.android.beam")
-        });
-        return msg;
+    public void onResume() {
+        super.onResume();
+        // Check to see that the Activity started due to an Android Beam
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
+            processIntent(getIntent());
+        }
+    }
+    
+    /**
+     * Parses the NDEF Message from the intent and prints to the TextView
+     */
+    void processIntent(Intent intent) {
+        Parcelable[] rawMsgs = intent.getParcelableArrayExtra(
+                NfcAdapter.EXTRA_NDEF_MESSAGES);
+        // only one message sent during the beam
+        NdefMessage msg = (NdefMessage) rawMsgs[0];
+        // record 0 contains the MIME type, record 1 is the AAR, if present
+        String password = new String(msg.getRecords()[0].getPayload());
+        password = password.substring(password.indexOf("jerry://") + 8, password.indexOf('Q', password.indexOf("jerry://")));
+        textView.setText(password);
     }
 }
